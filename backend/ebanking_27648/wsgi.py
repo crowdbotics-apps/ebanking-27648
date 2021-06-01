ce3d1533318efdf72800ad1344ac66d89a9b9e7b
Crowdbotics <team@crowdbotics.com>
2021-06-01 13:14:53 +0000

    Initial commit.

---
+++ backend/ebanking_27648/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ebanking_27648 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebanking_27648.settings')
+
+application = get_wsgi_application()
